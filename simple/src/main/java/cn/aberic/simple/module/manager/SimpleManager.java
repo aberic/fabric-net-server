faf81dc92e23799bfb0fcaab3f8426d14440d25b
abericyang <abericyang@gmail.com>
2018-06-06 11:49:32 +0800

    组织数据改由数据库获取

--- simple/src/main/java/cn/aberic/simple/module/manager/SimpleManager.java
+++ simple/src/main/java/cn/aberic/simple/module/manager/SimpleManager.java
@@ -1,8 +1,16 @@
 package cn.aberic.simple.module.manager;
 
 import cn.aberic.simple.base.BaseManager;
+import cn.aberic.simple.module.dto.OrdererDTO;
+import cn.aberic.simple.module.dto.OrgDTO;
+import cn.aberic.simple.module.dto.PeerDTO;
+import cn.aberic.simple.module.mapper.SimpleMapper;
 import org.hyperledger.fabric.sdk.aberic.FabricManager;
 import org.hyperledger.fabric.sdk.aberic.OrgManager;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 描述：
@@ -13,9 +21,9 @@ public class SimpleManager extends BaseManager {
 
     private static SimpleManager instance;
 
-    private FabricManager fabricManager;
+    private final Map<Integer, FabricManager> fabricManagerMap;
 
-    public static SimpleManager obtain() throws Exception {
+    public static SimpleManager obtain() {
         if (null == instance) {
             synchronized (SimpleManager.class) {
                 if (null == instance) {
@@ -26,33 +34,47 @@ public class SimpleManager extends BaseManager {
         return instance;
     }
 
-    private SimpleManager() throws Exception {
-        fabricManager = obtainFabricManager();
+    private SimpleManager() {
+        fabricManagerMap = new HashMap<>();
     }
 
-    public FabricManager getFabricManager() {
+    public FabricManager get(SimpleMapper simpleMapper, int orgId) throws Exception {
+        // 尝试从缓存中获取fabricManager
+        FabricManager fabricManager = fabricManagerMap.get(orgId);
+        if (null == fabricManager) { // 如果不存在fabricManager则尝试新建一个并放入缓存
+            synchronized (fabricManagerMap) {
+                OrgDTO org = simpleMapper.getOrgById(orgId);
+                if (null != org) {
+                    fabricManager = createFabricManager(org, simpleMapper.getOrdererByOrgId(orgId), simpleMapper.getPeerByOrgId(orgId));
+                    fabricManagerMap.put(orgId, fabricManager);
+                }
+            }
+        }
         return fabricManager;
     }
 
-    private FabricManager obtainFabricManager() throws Exception {
+    private FabricManager createFabricManager(OrgDTO org, List<OrdererDTO> orderers, List<PeerDTO> peers) throws Exception {
         OrgManager orgManager = new OrgManager();
         orgManager
-                .init("Org1", true, false)
-                .setUser("Admin", getCryptoConfigPath("aberic"), getChannleArtifactsPath("aberic"))
-//                .setUser("haha", "mAtBqOymDtBI", "org1.department1", new HashSet<>(Arrays.asList("hf.Revoker", "hf.GenCRL", "admin")), getCryptoConfigPath("aberic"))
-                .setCA("ca", "http://118.89.243.236:7054")
-                .setPeers("Org1MSP", "org1.example.com")
-                .addPeer("peer0.org1.example.com", "peer0.org1.example.com", "grpc://118.89.243.236:7051", "grpc://118.89.243.236:7053", true)
-                .setOrderers("example.com")
-                .addOrderer("orderer.example.com", "grpc://118.89.243.236:7050")
-                .setChannel("mychannel")
-                .setChainCode("test2cc", "/code", "chaincode/chaincode_example02", "1.2", 90000, 120)
+                .init(org.getId(), org.isTls(), org.isCaTls())
+                .setUser(org.getUsername(), getCryptoConfigPath(org.getCryptoConfigDir()), getChannleArtifactsPath(org.getChannelArtifactsDir()))
+                .setCA(org.getCaName(), org.getCaLocation())
+                .setPeers(org.getOrgName(), org.getOrgMSPID(), org.getOrgDomainName())
+                .setOrderers(org.getOrdererDomainName())
+                .setChannel(org.getChannelName())
+                .setChainCode(org.getChaincodeName(), org.getChaincodeSource(), org.getChaincodePath(), org.getChaincodeVersion(), org.getProposalWaitTime(), org.getInvokeWaitTime())
                 .setBlockListener(map -> {
                     logger.debug(map.get("code"));
                     logger.debug(map.get("data"));
-                })
-                .add();
-        return orgManager.use("Org1");
+                });
+        for (OrdererDTO orderer : orderers) {
+            orgManager.addOrderer(orderer.getName(), orderer.getLocation());
+        }
+        for (PeerDTO peer : peers) {
+            orgManager.addPeer(peer.getPeerName(), peer.getPeerEventHubName(), peer.getPeerLocation(), peer.getPeerEventHubLocation(), peer.isEventListener());
+        }
+        orgManager.add();
+        return orgManager.use(org.getId());
     }
 
 }
