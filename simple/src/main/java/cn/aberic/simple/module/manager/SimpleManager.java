4c2c45a139420b5b418e029998fe0d8382d2fdf1
abericyang <abericyang@gmail.com>
2018-06-08 11:17:49 +0800

    修改多节点组网为单节点

--- simple/src/main/java/cn/aberic/simple/module/manager/SimpleManager.java
+++ simple/src/main/java/cn/aberic/simple/module/manager/SimpleManager.java
@@ -1,27 +1,27 @@
 package cn.aberic.simple.module.manager;
 
-import cn.aberic.simple.base.BaseManager;
 import cn.aberic.simple.module.dto.OrdererDTO;
 import cn.aberic.simple.module.dto.OrgDTO;
 import cn.aberic.simple.module.dto.PeerDTO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hyperledger.fabric.sdk.aberic.FabricManager;
 import org.hyperledger.fabric.sdk.aberic.OrgManager;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 描述：
  *
  * @author : Aberic 【2018/6/4 10:46】
  */
-public class SimpleManager extends BaseManager {
+public class SimpleManager {
+
+    private Logger logger = LogManager.getLogger(SimpleManager.class);
 
     private static SimpleManager instance;
 
     private OrgDTO org;
-    private List<OrdererDTO> orderers;
-    private List<PeerDTO> peers;
+    private OrdererDTO orderer;
+    private PeerDTO peer;
     private FabricManager fabricManager;
 
     public static SimpleManager obtain() {
@@ -37,35 +37,36 @@ public class SimpleManager extends BaseManager {
 
     public void setOrg(OrgDTO org) {
         this.org = org;
+        fabricManager = null;
     }
 
-    public void addOrderer(OrdererDTO orderer) {
-        orderers.add(orderer);
+    public void setOrderer(OrdererDTO orderer) {
+        this.orderer = orderer;
+        fabricManager = null;
     }
 
-    public void addPeer(PeerDTO peer) {
-        peers.add(peer);
+    public void setPeer(PeerDTO peer) {
+        this.peer = peer;
+        fabricManager = null;
     }
 
     private SimpleManager() {
-        orderers = new ArrayList<>();
-        peers = new ArrayList<>();
     }
 
     public FabricManager get() throws Exception {
-        if (null == fabricManager) { // 如果不存在fabricManager则尝试新建一个并放入缓存
+        if (null == fabricManager) {
             synchronized (SimpleManager.class) {
-                fabricManager = createFabricManager(org, orderers, peers);
+                fabricManager = createFabricManager(org, orderer, peer);
             }
         }
         return fabricManager;
     }
 
-    private FabricManager createFabricManager(OrgDTO org, List<OrdererDTO> orderers, List<PeerDTO> peers) throws Exception {
+    private FabricManager createFabricManager(OrgDTO org, OrdererDTO orderer, PeerDTO peer) throws Exception {
         OrgManager orgManager = new OrgManager();
         orgManager
                 .init(org.getId(), org.isTls(), org.isCaTls())
-                .setUser(org.getUsername(), getCryptoConfigPath(org.getCryptoConfigDir()), getChannleArtifactsPath(org.getChannelArtifactsDir()))
+                .setUser(org.getUsername(), org.getCryptoConfigDir(), org.getChannelArtifactsDir())
                 .setCA(org.getCaName(), org.getCaLocation())
                 .setPeers(org.getOrgName(), org.getOrgMSPID(), org.getOrgDomainName())
                 .setOrderers(org.getOrdererDomainName())
@@ -75,12 +76,8 @@ public class SimpleManager extends BaseManager {
                     logger.debug(map.get("code"));
                     logger.debug(map.get("data"));
                 });
-        for (OrdererDTO orderer : orderers) {
-            orgManager.addOrderer(orderer.getName(), orderer.getLocation());
-        }
-        for (PeerDTO peer : peers) {
-            orgManager.addPeer(peer.getPeerName(), peer.getPeerEventHubName(), peer.getPeerLocation(), peer.getPeerEventHubLocation(), peer.isEventListener());
-        }
+        orgManager.addOrderer(orderer.getName(), orderer.getLocation());
+        orgManager.addPeer(peer.getPeerName(), peer.getPeerEventHubName(), peer.getPeerLocation(), peer.getPeerEventHubLocation(), peer.isEventListener());
         orgManager.add();
         return orgManager.use(org.getId());
     }
