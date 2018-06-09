6045f5e069c829799482a3f1c821548d31930d7a
Aberic <100309595@qq.com>
2018-06-09 16:19:50 +0800

    支持多节点管理

--- simple/src/main/java/cn/aberic/simple/module/manager/SimpleManager.java
+++ simple/src/main/java/cn/aberic/simple/module/manager/SimpleManager.java
@@ -3,10 +3,15 @@ package cn.aberic.simple.module.manager;
 import cn.aberic.simple.module.dto.OrdererDTO;
 import cn.aberic.simple.module.dto.OrgDTO;
 import cn.aberic.simple.module.dto.PeerDTO;
+import cn.aberic.simple.module.mapper.SimpleMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hyperledger.fabric.sdk.aberic.FabricManager;
 import org.hyperledger.fabric.sdk.aberic.OrgManager;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 描述：
@@ -19,10 +24,7 @@ public class SimpleManager {
 
     private static SimpleManager instance;
 
-    private OrgDTO org;
-    private OrdererDTO orderer;
-    private PeerDTO peer;
-    private FabricManager fabricManager;
+    private final Map<String, FabricManager> fabricManagerMap;
 
     public static SimpleManager obtain() {
         if (null == instance) {
@@ -35,37 +37,30 @@ public class SimpleManager {
         return instance;
     }
 
-    public void setOrg(OrgDTO org) {
-        this.org = org;
-        fabricManager = null;
-    }
-
-    public void setOrderer(OrdererDTO orderer) {
-        this.orderer = orderer;
-        fabricManager = null;
-    }
-
-    public void setPeer(PeerDTO peer) {
-        this.peer = peer;
-        fabricManager = null;
-    }
-
     private SimpleManager() {
+        fabricManagerMap = new HashMap<>();
     }
 
-    public FabricManager get() throws Exception {
-        if (null == fabricManager) {
-            synchronized (SimpleManager.class) {
-                fabricManager = createFabricManager(org, orderer, peer);
+    public FabricManager get(SimpleMapper simpleMapper, String orgHash) throws Exception {
+        // 尝试从缓存中获取fabricManager
+        FabricManager fabricManager = fabricManagerMap.get(orgHash);
+        if (null == fabricManager) { // 如果不存在fabricManager则尝试新建一个并放入缓存
+            synchronized (fabricManagerMap) {
+                OrgDTO org = simpleMapper.getOrgByHash(orgHash);
+                if (null != org) {
+                    fabricManager = createFabricManager(org, simpleMapper.getOrdererListByOrgHash(orgHash), simpleMapper.getPeerListByOrgHash(orgHash));
+                    fabricManagerMap.put(orgHash, fabricManager);
+                }
             }
         }
         return fabricManager;
     }
 
-    private FabricManager createFabricManager(OrgDTO org, OrdererDTO orderer, PeerDTO peer) throws Exception {
+
+    private FabricManager createFabricManager(OrgDTO org, List<OrdererDTO> orderers, List<PeerDTO> peers) throws Exception {
         OrgManager orgManager = new OrgManager();
         orgManager
-                .init(org.getId(), org.isTls(), org.isCaTls())
+                .init(org.getHash(), org.isTls() == 1, org.isCaTls() == 1)
                 .setUser(org.getUsername(), org.getCryptoConfigDir(), org.getChannelArtifactsDir())
                 .setCA(org.getCaName(), org.getCaLocation())
                 .setPeers(org.getOrgName(), org.getOrgMSPID(), org.getOrgDomainName())
@@ -76,10 +71,14 @@ public class SimpleManager {
                     logger.debug(map.get("code"));
                     logger.debug(map.get("data"));
                 });
-        orgManager.addOrderer(orderer.getName(), orderer.getLocation());
-        orgManager.addPeer(peer.getPeerName(), peer.getPeerEventHubName(), peer.getPeerLocation(), peer.getPeerEventHubLocation(), peer.isEventListener());
+        for (OrdererDTO orderer : orderers) {
+            orgManager.addOrderer(orderer.getName(), orderer.getLocation());
+        }
+        for (PeerDTO peer : peers) {
+            orgManager.addPeer(peer.getPeerName(), peer.getPeerEventHubName(), peer.getPeerLocation(), peer.getPeerEventHubLocation(), peer.isEventListener() == 1);
+        }
         orgManager.add();
-        return orgManager.use(org.getId());
+        return orgManager.use(org.getHash());
     }
 
 }
