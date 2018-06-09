62070a9116108aaa63d36dfb43837d4559e5ecde
Aberic <100309595@qq.com>
2018-06-09 16:15:30 +0800

    规范出入参数

--- simple/src/main/java/cn/aberic/simple/base/BaseService.java
+++ simple/src/main/java/cn/aberic/simple/base/BaseService.java
@@ -1,55 +1,52 @@
 package cn.aberic.simple.base;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 /**
  * 业务层基类
- * 
+ *
  * @author 杨毅 【2017年10月24日 - 20:58:52】
  */
 public interface BaseService {
 
-	Logger logger = LogManager.getLogger(BaseService.class);
+    int SUCCESS = 200;
+    int FAIL = 9999;
 
-	int SUCCESS = 200;
-	int FAIL = 40029;
-	int UN_LOGIN = 8;
+    default String responseSuccess(String result) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", SUCCESS);
+        jsonObject.put("data", result);
+        return jsonObject.toString();
+    }
 
-	default String responseSuccess(String result) {
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("status", SUCCESS);
-		jsonObject.put("result", result);
-		return jsonObject.toString();
-	}
+    default String responseSuccess(String result, String txid) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", SUCCESS);
+        jsonObject.put("data", result);
+        jsonObject.put("txid", txid);
+        return jsonObject.toString();
+    }
 
-	default String responseSuccess(String result, String txid) {
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("status", SUCCESS);
-		jsonObject.put("result", result);
-		jsonObject.put("txid", txid);
-		return jsonObject.toString();
-	}
+    default String responseSuccess(JSONObject json) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", SUCCESS);
+        jsonObject.put("data", json);
+        return jsonObject.toString();
+    }
 
-	default String responseSuccess(JSONObject json) {
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("status", SUCCESS);
-		jsonObject.put("data", json);
-		return jsonObject.toString();
-	}
+    default String responseSuccess(JSONArray array) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", SUCCESS);
+        jsonObject.put("data", array);
+        return jsonObject.toString();
+    }
 
-	default String responseFail(String result) {
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("status", FAIL);
-		jsonObject.put("error", result);
-		return jsonObject.toString();
-	}
-
-	default String responseUnLogin() {
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("status", UN_LOGIN);
-		return jsonObject.toString();
-	}
+    default String responseFail(String result) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", FAIL);
+        jsonObject.put("error", result);
+        return jsonObject.toString();
+    }
 
 }
