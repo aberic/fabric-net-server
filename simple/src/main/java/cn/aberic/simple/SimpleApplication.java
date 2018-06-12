8f054a18083e51d94fd091ef3b0df794462218e0
abericyang <abericyang@gmail.com>
2018-06-12 18:01:38 +0800

    更新

--- simple/src/main/java/cn/aberic/simple/SimpleApplication.java
+++ simple/src/main/java/cn/aberic/simple/SimpleApplication.java
@@ -2,6 +2,12 @@ package cn.aberic.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.Log4jConfigListener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
 
 @SpringBootApplication
 public class SimpleApplication {
@@ -9,4 +15,5 @@ public class SimpleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SimpleApplication.class, args);
     }
+
 }
