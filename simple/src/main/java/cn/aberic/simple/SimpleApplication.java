85d0071670752920b36823d25c5b9f22001a7629
Aberic <100309595@qq.com>
2018-06-12 23:11:27 +0800

    删除冗余依赖

--- simple/src/main/java/cn/aberic/simple/SimpleApplication.java
+++ simple/src/main/java/cn/aberic/simple/SimpleApplication.java
@@ -2,12 +2,6 @@ package cn.aberic.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.util.Log4jConfigListener;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
 
 @SpringBootApplication
 public class SimpleApplication {
