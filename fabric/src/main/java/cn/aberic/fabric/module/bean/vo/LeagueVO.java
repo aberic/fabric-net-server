a1dbdf440926b6a4d5c6bc4ce7cade7891d0754d
Aberic <100309595@qq.com>
2018-06-18 12:33:34 +0800

    新增前端联盟对象

---
+++ fabric/src/main/java/cn/aberic/fabric/module/bean/vo/LeagueVO.java
@@ -0,0 +1,63 @@
+package cn.aberic.fabric.module.bean.vo;
+
+import cn.aberic.fabric.base.BaseDTO;
+import io.swagger.annotations.ApiModel;
+
+/**
+ * 作者：Aberic on 2018/6/14 00:07
+ * 邮箱：abericyang@gmail.com
+ */
+@ApiModel(value = "联盟对象", description = "联盟对象信息")
+public class LeagueVO extends BaseDTO {
+
+    /** 当前联盟唯一id */
+    private int id;
+    /** 当前联盟名称 */
+    private String name;
+    /** 当前联盟创建日期 */
+    private String date;
+    /** 当前联盟下组织数量 */
+    private int count;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "LeagueVO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", date='" + date + '\'' +
+                ", count=" + count +
+                '}';
+    }
+}
