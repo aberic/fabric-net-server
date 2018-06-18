966d0036ef239b86c4b1738a5ad70276f857dfee
Aberic <100309595@qq.com>
2018-06-18 12:30:46 +0800

    新增前端联盟集合获取

--- fabric/src/main/java/cn/aberic/fabric/module/service/LeagueService.java
+++ fabric/src/main/java/cn/aberic/fabric/module/service/LeagueService.java
@@ -3,6 +3,9 @@ package cn.aberic.fabric.module.service;
 import cn.aberic.fabric.base.BaseService;
 import cn.aberic.fabric.base.DTOService;
 import cn.aberic.fabric.module.bean.dto.LeagueDTO;
+import cn.aberic.fabric.module.bean.vo.LeagueVO;
+
+import java.util.List;
 
 /**
  * 描述：
@@ -10,5 +13,7 @@ import cn.aberic.fabric.module.bean.dto.LeagueDTO;
  * @author : Aberic 【2018/6/4 10:27】
  */
 public interface LeagueService extends BaseService,DTOService<LeagueDTO> {
+
+    List<LeagueVO> leagues();
 
 }
