afea8723d1af066754d86c518cbabad6719a5786
Aberic <100309595@qq.com>
2018-06-22 22:30:37 +0800

    新增首页服务实现

--- fabric-edge/src/main/java/cn/aberic/fabric/controller/CommonController.java
+++ fabric-edge/src/main/java/cn/aberic/fabric/controller/CommonController.java
@@ -1,6 +1,7 @@
 package cn.aberic.fabric.controller;
 
 import cn.aberic.fabric.thrift.MultiServiceProvider;
+import cn.aberic.thrift.common.SystemInfo;
 import org.apache.thrift.TException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,7 @@ public class CommonController {
     private MultiServiceProvider multiService;
 
     @GetMapping(value = "index")
-    public ModelAndView leagueAdd() {
+    public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("index");
         int leagueCount = 0;
         int orgCount = 0;
@@ -30,14 +31,23 @@ public class CommonController {
         int peerCount = 0;
         int channelCount = 0;
         int chaincodeCount = 0;
+        SystemInfo systemInfo;
         try {
+            if (!multiService.getSystemService().isInit()) {
+                multiService.getSystemService().init();
+            }
             leagueCount = multiService.getLeagueService().listAll().size();
             orgCount = multiService.getOrgService().count();
             ordererCount = multiService.getOrdererService().count();
             peerCount = multiService.getPeerService().count();
             channelCount = multiService.getChannelService().count();
             chaincodeCount = multiService.getChaincodeService().count();
+            systemInfo = multiService.getSystemService().get();
+            systemInfo.setCpu((double) Math.round(systemInfo.getCpu() * 100) / 100);
+            systemInfo.setMemory((double) Math.round(systemInfo.getMemory() * 100) / 100);
+            systemInfo.setSwap((double) Math.round(systemInfo.getSwap() * 100) / 100);
         } catch (TException e) {
+            systemInfo = new SystemInfo();
             e.printStackTrace();
         }
         modelAndView.addObject("leagueCount", leagueCount);
@@ -46,6 +56,8 @@ public class CommonController {
         modelAndView.addObject("peerCount", peerCount);
         modelAndView.addObject("channelCount", channelCount);
         modelAndView.addObject("chaincodeCount", chaincodeCount);
+        modelAndView.addObject("systemInfo", systemInfo);
+
         return modelAndView;
     }
 }
