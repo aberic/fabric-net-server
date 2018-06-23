640a2222f370e17a825376f9651d0985e3a4fc27
Aberic <100309595@qq.com>
2018-06-23 16:51:55 +0800

    录入非空及配置附件管理

--- fabric-service/src/main/java/cn/aberic/fabric/service/OrgServiceImpl.java
+++ fabric-service/src/main/java/cn/aberic/fabric/service/OrgServiceImpl.java
@@ -1,13 +1,19 @@
 package cn.aberic.fabric.service;
 
+import cn.aberic.fabric.mapper.LeagueMapper;
 import cn.aberic.fabric.mapper.OrgMapper;
 import cn.aberic.fabric.utils.DateUtil;
+import cn.aberic.fabric.utils.FileUtil;
 import cn.aberic.thrift.org.OrgInfo;
 import cn.aberic.thrift.org.OrgService;
 import org.apache.thrift.TException;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
+import java.io.*;
+import java.nio.ByteBuffer;
 import java.util.List;
 
 @Service("orgService")
@@ -15,16 +21,35 @@ public class OrgServiceImpl implements OrgService.Iface {
 
     @Resource
     private OrgMapper orgMapper;
+    @Resource
+    private Environment env;
+    @Resource
+    private LeagueMapper leagueMapper;
 
 
     @Override
-    public int add(OrgInfo orgInfo) throws TException {
+    public int add(OrgInfo orgInfo, ByteBuffer buff, String fileName) throws TException {
+        if (StringUtils.isEmpty(orgInfo.getName()) ||
+                StringUtils.isEmpty(orgInfo.getMspId()) ||
+                StringUtils.isEmpty(orgInfo.getDomainName()) ||
+                StringUtils.isEmpty(orgInfo.getOrdererDomainName()) ||
+                StringUtils.isEmpty(orgInfo.getUsername())) {
+            return 0;
+        }
         orgInfo.setDate(DateUtil.getCurrent("yyyy年MM月dd日"));
+        String path = String.format("%s/%s/%s", env.getProperty("config.dir"), leagueMapper.get(orgInfo.getLeagueId()).getName(), orgInfo.getName());
+        orgInfo.setCryptoConfigDir(path + File.separator + fileName.split("\\.")[0]);
+        saveConfig(buff, orgInfo, fileName, path);
         return orgMapper.add(orgInfo);
     }
 
     @Override
-    public int update(OrgInfo orgInfo) throws TException {
+    public int update(OrgInfo orgInfo, ByteBuffer buff, String fileName) throws TException {
+        if (null != buff) {
+            String path = String.format("%s/%s/%s", env.getProperty("config.dir"), leagueMapper.get(orgInfo.getLeagueId()).getName(), orgInfo.getName());
+            orgInfo.setCryptoConfigDir(path + File.separator + fileName.split("\\.")[0]);
+            saveConfig(buff, orgInfo, fileName, path);
+        }
         return orgMapper.update(orgInfo);
     }
 
@@ -51,5 +76,36 @@ public class OrgServiceImpl implements OrgService.Iface {
     @Override
     public int count() throws TException {
         return orgMapper.countAll();
+    }
+
+    private void saveConfig(ByteBuffer buff, OrgInfo org, String fileName, String path) throws TException {
+        InputStream is = new ByteArrayInputStream(buff.array());
+        OutputStream os = null;
+        int bytesRead = 0;
+        byte[] buffer = new byte[8192];
+        try {
+            String unZipFile = path + File.separator + fileName;
+            File zipFile = new File(path + File.separator + fileName);
+            if (zipFile.getParentFile().exists()) {
+                zipFile.getParentFile().delete();
+            }
+            zipFile.getParentFile().mkdirs();
+            os = new FileOutputStream(zipFile);
+            while ((bytesRead = is.read(buffer, 0, 8192)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            FileUtil.unZipAndSave(unZipFile, path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+                if (null != os) {
+                    os.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
