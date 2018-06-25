a577ec7c0aeb7b0fc1ecb3f2f2f8c443fa7c6e7c
abericyang@gmail.com <yy81698989>
2018-06-25 15:34:29 +0800

    chaincode新增安装、实例化接口

--- fabric-service-api/thrift/chaincode.thrift
+++ fabric-service-api/thrift/chaincode.thrift
@@ -3,20 +3,25 @@ namespace java cn.aberic.thrift.chaincode
 struct ChaincodeInfo {
     1:i32 id,
     2:required string name,
-    3:required string path,
-    4:required string version,
-    5:required i32 proposalWaitTime,
-    6:required i32 invokeWaitTime,
-    7:required i32 channelId,
-    8:optional string date,
-    9:optional string channelName,
-    10:optional string peerName,
-    11:optional string orgName,
-    12:optional string leagueName
+    3:optional string source,
+    4:optional string path,
+    5:required string version,
+    6:required i32 proposalWaitTime,
+    7:required i32 invokeWaitTime,
+    8:required i32 channelId,
+    9:optional string date,
+    10:optional string channelName,
+    11:optional string peerName,
+    12:optional string orgName,
+    13:optional string leagueName
 }
 service ChaincodeService {
 
     i32 add(1:ChaincodeInfo chaincodeInfo);
+
+    string install(1:ChaincodeInfo chaincodeInfo, 2:binary buff, 3:string fileName);
+
+    string instantiate(1:ChaincodeInfo chaincodeInfo, 2:list<string> strArray)
 
     i32 update(1:ChaincodeInfo chaincodeInfo);
 
