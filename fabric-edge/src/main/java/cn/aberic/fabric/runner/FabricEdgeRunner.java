60209f4df30c105a157cbe263543072f3a37ab36
Aberic <100309595@qq.com>
2018-07-02 23:40:04 +0800

    用户初始化

--- fabric-edge/src/main/java/cn/aberic/fabric/runner/FabricEdgeRunner.java
+++ fabric-edge/src/main/java/cn/aberic/fabric/runner/FabricEdgeRunner.java
@@ -1,8 +1,13 @@
 package cn.aberic.fabric.runner;
 
+import cn.aberic.fabric.dao.User;
+import cn.aberic.fabric.dao.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * 作者：Aberic on 2018/6/22 21:36
@@ -11,8 +16,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class FabricEdgeRunner implements ApplicationRunner {
 
+    @Resource
+    private UserMapper userMapper;
+
+    @Value("${username}")
+    private String username;
+    @Value("${password}")
+    private String password;
+
     @Override
     public void run(ApplicationArguments args) {
+        addUser();
         System.out.println();
         System.out.println(" _____   _   _   ____    ");
         System.out.println("| ____| | \\ | | |  _ \\   ");
@@ -22,6 +36,15 @@ public class FabricEdgeRunner implements ApplicationRunner {
         System.out.println();
         System.out.println("===================== please make your fabric net work ===================== ");
         System.out.println();
-        System.out.println("=============================== read sdk logs ============================== ");
+        System.out.println("=============================== read logs ============================== ");
+    }
+
+    private void addUser() {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+//        user.setUsername(System.getenv("USERNAME"));
+//        user.setPassword(System.getenv("PASSWORD"));
+        userMapper.add(user);
     }
 }
