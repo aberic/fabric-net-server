73f37a80ab926cb58992f6cae43e9eb4999f0166
abericyang@gmail.com <yy81698989>
2018-07-03 11:29:01 +0800

    缓存策略

---
+++ fabric-edge/src/main/java/cn/aberic/fabric/utils/CacheUtil.java
@@ -0,0 +1,25 @@
+package cn.aberic.fabric.utils;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+public class CacheUtil {
+
+    private static Cache<String, String> cacheString = CacheBuilder.newBuilder().maximumSize(5)
+            .expireAfterWrite(30, TimeUnit.MINUTES).build();
+
+    public static void put(String key, String value) {
+        cacheString.put(key, value);
+    }
+
+    public static String get(String key) {
+        try {
+            return cacheString.getIfPresent(key);
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+}
