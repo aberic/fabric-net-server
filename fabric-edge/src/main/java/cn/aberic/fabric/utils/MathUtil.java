3dfaf170221212f07e1377f5f5a022073ac759dd
abericyang@gmail.com <yy81698989>
2018-07-06 16:39:28 +0800

    取随机数工具

---
+++ fabric-edge/src/main/java/cn/aberic/fabric/utils/MathUtil.java
@@ -0,0 +1,33 @@
+package cn.aberic.fabric.utils;
+
+
+import java.util.UUID;
+
+/**
+ * 描述：
+ *
+ * @author : Aberic 【2018-07-06 14:46】
+ */
+public class MathUtil {
+
+    private static String[] chars = new String[]{"a", "b", "c", "d", "e", "f",
+            "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+            "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+            "W", "X", "Y", "Z"};
+
+
+    public static String getRandom8() {
+        StringBuilder sb = new StringBuilder();
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        for (int i = 0; i < 8; i++) {
+            String str = uuid.substring(i * 4, i * 4 + 4);
+            int x = Integer.parseInt(str, 16);
+            sb.append(chars[x % 0x3E]);
+        }
+        return sb.toString();
+    }
+
+
+}
