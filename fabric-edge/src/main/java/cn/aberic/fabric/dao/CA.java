8bff602f5ae67ebaeda29e0ec8663913dbfd7cac
Aberic <100309595@qq.com>
2018-07-12 23:58:45 +0800

    新增CA对象

---
+++ fabric-edge/src/main/java/cn/aberic/fabric/dao/CA.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018. Aberic - All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.aberic.fabric.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 作者：Aberic on 2018/7/12 21:02
+ * 邮箱：abericyang@gmail.com
+ */
+@Setter
+@Getter
+public class CA {
+
+    private int id;
+    private String name;
+    private String skPath;
+    private String certificatePath;
+    private int peerId;
+    private String date; // optional
+    private String peerName; // optional
+    private String orgName; // optional
+    private String leagueName; // optional
+
+}
