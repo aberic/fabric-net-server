ded78edb60c167331eca46a1e5741c4069ec229f
Aberic <100309595@qq.com>
2018-07-15 21:32:36 +0800

    RC6脚本更新

--- init-edge.sh
+++ init-edge.sh
@@ -10,9 +10,9 @@ echo
 ## 启动SDK
 startEdge () {
     ## 启动spring-boot服务
-    nohup java -jar /home/fabric-edge-1.0-RC5.jar > /home/fabric-edge-1.0-RC5.nohup 2>&1 &
+    nohup java -jar /home/fabric-edge-1.0-RC6.jar > /home/fabric-edge-1.0-RC6.nohup 2>&1 &
 
-    tail -f logs /home/fabric-edge-1.0-RC5.nohup
+    tail -f logs /home/fabric-edge-1.0-RC6.nohup
 }
 
 startEdge
