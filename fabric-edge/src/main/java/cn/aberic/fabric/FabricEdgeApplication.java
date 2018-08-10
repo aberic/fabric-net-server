67c7ce5aed5a1d0ba1b9039937bd3f19d14e32c0
Aberic <100309595@qq.com>
2018-08-10 21:19:50 +0800

    新增定时任务注解

--- fabric-edge/src/main/java/cn/aberic/fabric/FabricEdgeApplication.java
+++ fabric-edge/src/main/java/cn/aberic/fabric/FabricEdgeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableScheduling
+@EnableScheduling
 public class FabricEdgeApplication {
 
     public static void main(String[] args) {
