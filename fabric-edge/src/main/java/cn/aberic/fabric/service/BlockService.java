24498fc5e2f0e0017b1cd526f32f448db47f59f6
abericyang@gmail.com <yy81698989>
2018-08-10 17:54:50 +0800

    区块业务接口

---
+++ fabric-edge/src/main/java/cn/aberic/fabric/service/BlockService.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2018. Aberic - All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.aberic.fabric.service;
+
+import cn.aberic.fabric.bean.*;
+import cn.aberic.fabric.dao.Block;
+import cn.aberic.fabric.dao.Channel;
+
+import java.util.List;
+
+/**
+ * 描述：
+ *
+ * @author : Aberic 【2018-08-10 16:23】
+ */
+public interface BlockService {
+
+    int add(Block block);
+
+    List<ChannelPercent> getChannelPercents(List<Channel> channels);
+
+    List<ChannelBlockList> getChannelBlockLists(List<Channel> channels);
+
+    DayStatistics getDayStatistics();
+
+    Platform getPlatform();
+
+}
